e0a77f76cd28d6db06de834afb7beb3200792891
jonmoon74 <jon.moon@outlook.com>
2018-05-31 20:48:25 +0100

    Reset to begin emoji dictionary project

--- PracticeApp1/ViewController.swift
+++ PracticeApp1/ViewController.swift
@@ -10,22 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var theLabel: UILabel!
-    
-    @IBOutlet weak var text1: UITextField!
-
-    @IBOutlet weak var text2: UITextField!
-    
-    
-    @IBAction func buttonTapped(_ sender: Any) {
-        
-        theLabel.text = "The answer is...\(Double(text1.text!)! + Double(text2.text!)!)"
-    }
-    
-    @IBAction func button2Tapped(_ sender: Any) {
-        theLabel.text = "Button2 pushed"
-        print("button 2 tapped")
-    }
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
